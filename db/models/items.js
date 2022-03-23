9a7561f3dc6cea1211de172bc73e15c48642ed38
mkang7446 <mkang7446@gmail.com>
2022-03-23 11:02:39 -0700

    fix category key in items.js

--- db/models/items.js
+++ db/models/items.js
@@ -1,18 +1,15 @@
 const mongoose = require('../connection');
 const Schema = mongoose.Schema;
 
+const ItemSchema = new Schema({
+  name: String,
+  image: String,
+  description: String,
+  price: String,
+  category: String,
+  _id: Number,
+});
 
-const ItemSchema = new Schema(
-    {
-        name: String,
-        image: String,
-        description: String,
-        price: String,
-        categories: String,
-        _id: Number
-    }
-)
-
-const Item = mongoose.model("Item", ItemSchema);
+const Item = mongoose.model('Item', ItemSchema);
 
 module.exports = Item;
