bb701907f595832fd6a9f5d3f3eecb146ba5a248
acourney <acourney@ucsc.edu>
2022-03-24 16:06:44 -0700

    add item to array in cart instead of adding new cart

--- db/models/carts.js
+++ db/models/carts.js
@@ -2,8 +2,7 @@ const mongoose = require("../connection");
 const path = require("path");
 
 const CartSchema = new mongoose.Schema({
-  id: 1,
-  item: [{ ref: "Item", type: mongoose.Schema.Types.ObjectId }],
+  item: [{ ref: "Item", type: Number }],
   quantity: 0,
 });
 
